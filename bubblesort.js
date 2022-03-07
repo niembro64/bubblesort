ec960135f545842882c20c6ee0ee2bc998643033
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-03-07 11:14:37 -0500

    Two Ways

--- bubblesort.js
+++ bubblesort.js
@@ -1,4 +1,4 @@
-function bubbleSort(arr) {
+function simpleSort(arr) {
     var temp;
 
     for (let x = 0; x < arr.length - 1; x++) {
@@ -12,12 +12,28 @@ function bubbleSort(arr) {
     }
     return arr;
 }
+function bubbleSort(arr) {
+    var temp;
+    let len = arr.length;
+    let checked;
+    do {
+        checked = false;
+        for (let i = 0; i < len; i++) {
+            if (arr[i] > arr[i + 1]) {
+                let temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                checked = true;
+            }
+        }
+    } while (checked);
+    return arr;
+}
 
-let arr = [5, 4, 1, 6, 8, 14, 1, 3];
-let arr1 = [15,5, 4, 1, 6, 8, 14, 1, 3,-11];
-let arr2 = [3,2,1];
+// let arr = [5, 4, 1, 6, 8, 14, 1, 3];
+let arr1 = [15, 5, 4, 1, 6, 8, 14, 1, 3, -11];
+let arr2 = [3, 2, 1];
 let arr3 = [];
 
-console.log(arr3);
-console.log(bubbleSort(arr3));
-
+console.log(arr1);
+console.log(bubbleSort(arr1));
