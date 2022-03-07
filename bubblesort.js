b423cb2402103c3ce067a0033f45f4fa9f14da39
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-03-07 10:23:08 -0500

    done

--- bubblesort.js
+++ bubblesort.js
@@ -1,9 +1,19 @@
-function bubbleSort(arr){
-
-    
+function bubbleSort(arr) {
+    var temp;
+    for (let x = 0; x < arr.length - 1; x++) {
+        for (let i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                temp = arr[i + 1];
+                arr[i + 1] = arr[i];
+                arr[i] = temp;
+            }
+        }
+    }
     return arr;
 }
 
-let arr = [5,4,1,6,8,14,1,3];
-console.log(arr);
-console.log(bubbleSort(arr));
+let arr = [5, 4, 1, 6, 8, 14, 1, 3];
+let arr1 = [15,5, 4, 1, 6, 8, 14, 1, 3,-11];
+console.log(arr1);
+console.log(bubbleSort(arr1));
+
