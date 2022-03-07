f7cb39795a77b61a033d1b7871a59947d53e14ce
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-03-07 10:24:59 -0500

    Tested Empty

--- bubblesort.js
+++ bubblesort.js
@@ -1,5 +1,6 @@
 function bubbleSort(arr) {
     var temp;
+
     for (let x = 0; x < arr.length - 1; x++) {
         for (let i = 0; i < arr.length - 1; i++) {
             if (arr[i] > arr[i + 1]) {
@@ -14,6 +15,9 @@ function bubbleSort(arr) {
 
 let arr = [5, 4, 1, 6, 8, 14, 1, 3];
 let arr1 = [15,5, 4, 1, 6, 8, 14, 1, 3,-11];
-console.log(arr1);
-console.log(bubbleSort(arr1));
+let arr2 = [3,2,1];
+let arr3 = [];
+
+console.log(arr3);
+console.log(bubbleSort(arr3));
 
